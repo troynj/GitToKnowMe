97aa91659d3bbe5ba1ec17f7236d88976ff48d55
troynj <troynjohnson6@gmail.com>
2023-01-19 00:39:56 -0800

    fixing styling

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,67 +4,77 @@ function writeProject(projectArr) {
   
   projectArr.forEach((el) => {
     var projectEl = document.createElement("article");
+    var contentEl = document.createElement("div");
+    var titleEl = document.createElement("h3");
     var repoEl = document.createElement("p");
-    var nameEl = document.createElement("h3");
-    var imageEl = document.createElement("img");
+    // var imageEl = document.createElement("div");
     var descEl = document.createElement("p");
+    repoEl.classList.add("repo")
+    descEl.classList.add("desc")
 
-    nameEl.addEventListener("click", () => window.open(el.deploy, "_blank"));
 
-    repoEl.textContent ="View Repository"
+
+    titleEl.addEventListener("click", () => window.open(el.deploy, "_blank"));
     repoEl.addEventListener("click", () => window.open(el.repo, "_blank"));
 
+    repoEl.textContent ="View Repository"
+    titleEl.textContent = el.title
+    descEl.textContent = el.desc
+    titleEl.textContent = el.image
+    contentEl.style.backgroundImage = `url(./assets/images/${el.image})`
+
+    // projectEl.style.backgroundColor = "red"
 
 
     projectsEl.appendChild(projectEl);
-    projectEl.appendChild(imageEl);
-    imageEl.appendChild(descEl);
-    descEl.appendChild(repoEl);
-    projectEl.appendChild(nameEl);
+    projectEl.appendChild(contentEl);
+    contentEl.appendChild(descEl);
+    contentEl.appendChild(repoEl);
+    projectEl.appendChild(titleEl);
   });
 }
 
 var myProjects = [
   {
-    name: "GitYourGameOn",
+    title: "GitYourGameOn",
     repo: "https://github.com/troynj/GitYourGameOn",
     deploy: "https://bradcoleman60.github.io/GitYourGameOn/",
-    image: "./assets/images/gitYourGameOn.gif",
+    image: "gitYourGameOn.gif",
     desc: "All the NBA data you need, from player stats to game times plus easy access to purchase tickets. Whats more - save your favorite players and view them all in one place",
   },
   {
-    name: "passwordGenerator",
+    title: "passwordGenerator",
     repo: "https://github.com/troynj/passwordGenerator",
     deploy: "https://troynj.github.io/passwordGenerator/",
-    image: "./assets/images/passwordGenerator.png",
+    image: "passwordGenerator.png",
     desc: "Recieve a randomized password after selecting parameters for your desired password!",
   },
   {
-    name: "dailyPlanner",
+    title: "dailyPlanner",
     repo: "https://github.com/troynj/dailyPlanner",
     deploy: "https://troynj.github.io/dailyPlanner/",
-    image: "./assets/images/dailyPlanner.png",
+    image: "dailyPlanner.png",
     desc: "You can create, view, and edit appointments for each hour of the work day. Your appointments will persist even after you reload the page!",
   },
   {
-    name: "QuizGame",
+    title: "QuizGame",
     repo: "https://github.com/troynj/QuizGame",
     deploy: "https://troynj.github.io/QuizGame/",
-    image: "./assets/images/quizGame.gif",
+    image: "quizGame.gif",
     desc: "Test your knowledge of everything code related with this code quiz game! view your scores and compete with your local friends!",
   },
   {
-    name: "weatherForecast",
+    title: "weatherForecast",
     repo: "https://github.com/troynj/weatherForecast",
     deploy: "https://troynj.github.io/weatherForecast/",
-    image: "./assets/images/weatherForecast.png",
+    image: "weatherForecast.png",
     desc: "Search a city - This website makes api calls to get the weather forecast for the current day and the future 5 days, plus the background image is updated with your searched city!",
   },
   {
-    name: "wordGuesser",
+    title: "wordGuesser",
     repo: "https://github.com/troynj/wordGuess",
     deploy: "https://troynj.github.io/wordGuess/",
-    image: "./assets/images/wordGuesser.png",
+    image: "wordGuesser.png",
     desc: "Guess the word before the time runs out!",
   },
 ];
